a9b95a793e3fc189117fc16cf1ab59e46914b260
RoBoSPL <82057954+RoBoSPL@users.noreply.github.com>
2025-03-12 00:24:24 +0100

    Update ads1110_sensor.h

--- components/ads1110/ads1110_sensor.h
+++ components/ads1110/ads1110_sensor.h
@@ -1,4 +1,3 @@
-// custom_components/ads1110_sensor.h
 #include "esphome.h"
 #include <Wire.h>
 
@@ -6,14 +5,18 @@ class ADS1110Sensor : public PollingComponent, public Sensor {
  public:
   ADS1110Sensor() : PollingComponent(1000) {}
 
+  void setup() override {
+    Wire.begin();
+  }
+
   void update() override {
-    Wire.requestFrom(0x48, 3);
+    Wire.requestFrom(0x48, 3);  // ADS1110 I2C address
     if (Wire.available() == 3) {
       uint8_t highbyte = Wire.read();
       uint8_t lowbyte = Wire.read();
-      uint8_t configRegister = Wire.read();
-      int16_t raw_value = (highbyte << 8) | lowbyte;  // Fixed the parenthesis here
-      float voltage = raw_value * 0.000125;
+      uint8_t configRegister = Wire.read();  // Read config register, but we can ignore it for now
+      int16_t raw_value = (highbyte << 8) | lowbyte;
+      float voltage = raw_value * 0.000125;  // Convert raw value to voltage
       publish_state(voltage);
     }
   }
